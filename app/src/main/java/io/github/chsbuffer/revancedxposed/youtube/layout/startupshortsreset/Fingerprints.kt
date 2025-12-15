ceba4610be9cd97b9433c9e4efc66a496e9fc6f2
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2025-12-15 11:13:23 +0800

    DisableResumingShortsOnStartup: Support YouTube `20.47.59`

--- app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/layout/startupshortsreset/Fingerprints.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/layout/startupshortsreset/Fingerprints.kt
@@ -1,14 +1,53 @@
 package io.github.chsbuffer.revancedxposed.youtube.layout.startupshortsreset
 
 import io.github.chsbuffer.revancedxposed.AccessFlags
+import io.github.chsbuffer.revancedxposed.Opcode
+import io.github.chsbuffer.revancedxposed.accessFlags
 import io.github.chsbuffer.revancedxposed.findMethodDirect
 import io.github.chsbuffer.revancedxposed.fingerprint
+import io.github.chsbuffer.revancedxposed.opcodes
+import io.github.chsbuffer.revancedxposed.parameters
+import io.github.chsbuffer.revancedxposed.returns
 
-val userWasInShortsFingerprint = fingerprint {
-    returns("V")
-    accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
-    parameters("Ljava/lang/Object;")
-    strings("userIsInShorts: ")
+val userWasInShortsFingerprint = findMethodDirect {
+    runCatching {
+        fingerprint {
+            returns("V")
+            accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+            parameters("Ljava/lang/Object;")
+            strings("userIsInShorts: ")
+        }
+    }.getOrElse {
+        findMethod {
+            matcher {
+                returns("V")
+                accessFlags(AccessFlags.PUBLIC, AccessFlags.FINAL)
+                parameters("Ljava/lang/Object;")
+                opcodes(
+                    Opcode.INVOKE_INTERFACE, // userWasInShortsProtoStoreProvider
+                    Opcode.MOVE_RESULT_OBJECT,
+                    Opcode.CHECK_CAST,
+                    Opcode.NEW_INSTANCE,
+                    Opcode.INVOKE_DIRECT, // userWasInShortsBuilder
+                    Opcode.INVOKE_INTERFACE,
+                    Opcode.RETURN_VOID,
+                )
+            }
+        }.findMethod {
+            matcher {
+                opcodes(
+                    Opcode.CHECK_CAST, // p1, Ljava/lang/Boolean; // userIsInShorts
+                    Opcode.INVOKE_VIRTUAL, // Ljava/lang/Boolean;->booleanValue()Z
+                    Opcode.MOVE_RESULT,
+                    Opcode.IGET_OBJECT,
+                    Opcode.MOVE_OBJECT,
+                    Opcode.CHECK_CAST,
+                    Opcode.IGET_OBJECT,
+                    Opcode.INVOKE_INTERFACE, // userWasInShortsProtoStoreProvider
+                )
+            }
+        }.single()
+    }
 }
 
 val userWasInShortsBuilderFingerprint = findMethodDirect {
